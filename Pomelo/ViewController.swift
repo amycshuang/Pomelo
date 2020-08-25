0237850e073431f64d651617cfd44bad68af7b24
Amy Huang <ach243@cornell.edu>
2020-08-25 15:35:02 -0500

    Initial Commit

---
+++ Pomelo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pomelo
+//
+//  Created by Amy Chin Siu Huang on 8/25/20.
+//  Copyright © 2020 Amy Chin Siu Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
